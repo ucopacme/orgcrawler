0d0ed39cb61dbd870fb3e19a993f89465cd0b0c7
Eric Odell <eric.odell@ucop.edu>
2018-07-26 19:05:01 -0700

    silly test for travis

--- organizer/hello_world.py
+++ organizer/hello_world.py
@@ -1,6 +1,7 @@
 """
 Starting place for initial unittest/travis setup
 """
+import datetime
 
 def hello_world():
     return 'hello world'
@@ -9,3 +10,6 @@ def hello_eric():
     return 'hello eric'
 
 
+def hello_now():
+    d = datetime.datetime.now()
+    return 'hello ' + str(d)
