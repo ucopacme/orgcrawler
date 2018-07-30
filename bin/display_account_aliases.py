8d30e0a5845a14254be34b0ea397aee2377b9ac0
Ashley Gould <agould@ucop.edu>
2018-07-30 08:03:10 -0700

    demo script display_account_aliases.py working
    runs in about 10 seconds

--- bin/display_account_aliases.py
+++ bin/display_account_aliases.py
@@ -17,18 +17,35 @@ from organizer import crawlers, orgs, utils
 def get_account_aliases(region, account):
     client = boto3.client('iam', region_name=region, **account.credentials)
     response = client.list_account_aliases()
-    return dict(AccountAliases=response['AccountAliases'])
+    return dict(Aliases=response['AccountAliases'])
+
+
+def initialize_crawler(role_name):
+    master_account_id = utils.get_master_account_id(role_name)
+    org = orgs.Org(master_account_id, role_name)
+    org.load()
+    crawler = crawlers.Crawler(org)
+    crawler.load_account_credentials()
+    return crawler
+
+
+def process_request_outputs(request):
+    accounts_with_aliases = []
+    for response in request.responses:
+        account = response.account.dump()
+        account['Alias'] = ', '.join(response.payload_output['Aliases'])
+        accounts_with_aliases.append(account)
+    return(utils.jsonfmt(accounts_with_aliases))
 
 
 def main():
     args = docopt(__doc__)
-    master_account_id = utils.get_master_account_id(args['ROLE'])
-    org = orgs.Org(master_account_id, args['ROLE'])
-    org.load()
-    crawler = crawlers.Crawler(org)
-    crawler.load_account_credentials()
-    crawler.execute(get_account_aliases)
-    print(utils.yamlfmt(crawler.get_payload_response_by_name('get_account_aliases').dump))
+    crawler = initialize_crawler(args['ROLE'])
+    print(crawler)
+    request = crawler.execute(get_account_aliases)
+    print(process_request_outputs(request))
+    print()
+    print(request.timer.elapsed_time)
 
 
 if __name__ == "__main__":
