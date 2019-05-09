ce5571715ad7b85025cf90abff30594783b9ae46
Ashley Gould <agould@ucop.edu>
2019-05-09 07:48:23 -0700

    payloads: refacter create_bucket(), add delete_bucket()
    add exception handling

--- test/test_payloads.py
+++ test/test_payloads.py
@@ -36,15 +36,47 @@ def test_get_set_account_aliases():
 @mock_sts
 @mock_organizations
 @mock_s3
-def test_create_list_buckets():
+def test_create_list_delete_buckets():
     org_id, root_id = build_mock_org(SIMPLE_ORG_SPEC)
     crawler = setup_crawler(ORG_ACCESS_ROLE)
     account = crawler.accounts[0]
     region = crawler.regions[0]
+
+    # test create and list
     response = payloads.create_bucket(region, account, 'test_bucket')
-    assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+    print(response)
+    assert response['CreateBucketOperation']['BucketName'] == '-'.join(['test_bucket', account.id, region])
+    assert response['CreateBucketOperation']['Succeeded'] == True
+    assert response['CreateBucketOperation']['HTTPStatusCode'] == 200
     response = payloads.list_buckets(region, account)
-    assert response['Buckets'][0] == 'test_bucket-' + account.id
+    print(response)
+    assert response['Buckets'][0] == '-'.join(['test_bucket', account.id, region])
+
+    # test error conditions
+    response = payloads.create_bucket(region, account, 'test_bucket')
+    print(response)
+    assert response['CreateBucketOperation']['BucketName'] == '-'.join(['test_bucket', account.id, region])
+    assert response['CreateBucketOperation']['Succeeded'] == False
+    assert response['CreateBucketOperation']['ErrorCode'] == 'BucketAlreadyExists'
+    response = payloads.delete_bucket(region, account, 'test_bucket')
+    print(response)
+    assert response['DeleteBucketOperation']['BucketName'] == '-'.join(['test_bucket', account.id, region])
+    assert response['DeleteBucketOperation']['Succeeded'] == True
+    assert response['DeleteBucketOperation']['HTTPStatusCode'] == 204
+    response = payloads.delete_bucket(region, account, 'test_bucket')
+    print(response)
+    assert response['DeleteBucketOperation']['BucketName'] == '-'.join(['test_bucket', account.id, region])
+    assert response['DeleteBucketOperation']['Succeeded'] == False
+    assert response['DeleteBucketOperation']['ErrorCode'] == 'NoSuchBucket'
+
+    # test edge case for location constraint
+    region = 'us-east-1'
+    response = payloads.create_bucket(region, account, 'test_bucket')
+    print(response)
+    assert response['CreateBucketOperation']['Succeeded'] == True
+    response = payloads.list_buckets(region, account)
+    print(response)
+    assert response['Buckets'][0] == '-'.join(['test_bucket', account.id, region])
  
 
 @mock_sts
