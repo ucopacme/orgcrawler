cf555398f4146d662b38d31b8545a038cd0f2521
Ashley Gould <agould@ucop.edu>
2019-05-17 10:13:10 -0700

    Release 1.0.0a2
    [Resolves #63] add support for service control policies
    [Resolves #64] add logging facility to help with debugging

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'botocore',
